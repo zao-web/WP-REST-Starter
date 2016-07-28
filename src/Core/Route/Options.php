b592d3264e2eb87b4890397711b5e2143978007b
tfrommen <info@tfrommen.de>
2016-07-28 17:23:23 +0200

    Adapt route options implementation.

--- src/Core/Route/Options.php
+++ src/Core/Route/Options.php
@@ -2,17 +2,22 @@
 
 namespace Inpsyde\WPRESTStarter\Core\Route;
 
-use Inpsyde\WPRESTStarter\Common;
-use Inpsyde\WPRESTStarter\Common\Endpoint;
+use Inpsyde\WPRESTStarter\Common\Arguments;
+use Inpsyde\WPRESTStarter\Common\Endpoint\Handler;
+use Inpsyde\WPRESTStarter\Common\Endpoint\RequestHandler;
+use Inpsyde\WPRESTStarter\Common\Endpoint\Schema;
+use Inpsyde\WPRESTStarter\Common\Route\ExtensibleOptions;
+use Inpsyde\WPRESTStarter\Common\Route\SchemaAwareOptions;
 use WP_REST_Server;
 
 /**
- * Route options implementation providing several named constructors and setters.
+ * Implementation of extensible and schema-aware route options.
  *
  * @package Inpsyde\WPRESTStarter\Core\Route
  * @since   1.0.0
+ * @since   1.1.0 Implement specific interfaces for extensible and schema-aware route options.
  */
-class Options implements Common\Arguments {
+class Options implements ExtensibleOptions, SchemaAwareOptions {
 
 	/**
 	 * Default comma-separated HTTP verbs.
@@ -47,22 +52,45 @@ class Options implements Common\Arguments {
 	 *
 	 * @since 1.0.0
 	 * @since 1.1.0 Make use of late static binding (i.e., return a new instance of `static` instead of `self`).
+	 * @since 1.1.0 Temporarily removed the `RequestHandler` type hint from the `$handler` parameter.
 	 *
-	 * @param Endpoint\Handler $handler Optional. Request handler object. Defaults to null.
-	 * @param Common\Arguments $args    Optional. Endpoint arguments object. Defaults to null.
-	 * @param string           $methods Optional. Comma-separated HTTP verbs. Defaults to self::DEFAULT_METHODS.
-	 * @param array            $options Optional. Additional options array. Defaults to empty array.
+	 * @param RequestHandler|Handler $handler Optional. Request handler object. Defaults to null.
+	 * @param Arguments              $args    Optional. Endpoint arguments object. Defaults to null.
+	 * @param string                 $methods Optional. Comma-separated HTTP verbs. Defaults to self::DEFAULT_METHODS.
+	 * @param array                  $options Optional. Additional options array. Defaults to empty array.
 	 *
 	 * @return static Route options object.
 	 */
 	public static function from_arguments(
-		Endpoint\Handler $handler = null,
-		Common\Arguments $args = null,
+		$handler = null,
+		Arguments $args = null,
 		$methods = self::DEFAULT_METHODS,
 		array $options = []
 	) {
 
-		return ( new static() )->add_from_arguments( $handler, $args, $methods, $options );
+		// TODO: With version 2.0.0, adapt both the doc and type hint of $handler (i.e., RequestHandler only).
+
+		// TODO: With version 2.0.0, remove the following block.
+		if ( ! $handler instanceof RequestHandler ) {
+			_doing_it_wrong(
+				__METHOD__,
+				"The request handler, passed as first argument, should be an instance of Inpsyde\\WPRESTStarter\\Common\\Endpoint\\RequestHandler.",
+				'1.1.0'
+			);
+		}
+
+		if ( $handler ) {
+			// TODO: With version 2.0.0, just use `'handle_request'`.
+			$options['callback'] = [ $handler, $handler instanceof RequestHandler ? 'handle_request' : 'process' ];
+		}
+
+		if ( $args ) {
+			$options['args'] = $args->to_array();
+		}
+
+		$options['methods'] = (string) $methods;
+
+		return new static( $options );
 	}
 
 	/**
@@ -80,7 +108,7 @@ class Options implements Common\Arguments {
 	 */
 	public static function with_callback(
 		$callback,
-		$args = [],
+		array $args = [],
 		$methods = self::DEFAULT_METHODS,
 		array $options = []
 	) {
@@ -94,28 +122,29 @@ class Options implements Common\Arguments {
 	 * @since 1.0.0
 	 * @since 1.1.0 Make use of late static binding (i.e., return a new instance of `static` instead of `self`).
 	 *
-	 * @param Endpoint\Schema $schema  Schema object.
-	 * @param array           $options Optional. Route options. Defaults to empty array.
+	 * @param Schema $schema  Schema object.
+	 * @param array  $options Optional. Route options. Defaults to empty array.
 	 *
 	 * @return static Route options object.
 	 */
-	public static function with_schema( Endpoint\Schema $schema, array $options = [] ) {
+	public static function with_schema( Schema $schema, array $options = [] ) {
 
 		return ( new static( $options ) )->set_schema( $schema );
 	}
 
 	/**
-	 * Adds the given array to the options.
+	 * Adds the given options object or array as new entry to the internal options.
 	 *
 	 * @since 1.0.0
+	 * @since 1.1.0 Removed the array type hint from the $options parameter.
 	 *
-	 * @param array $options Options array.
+	 * @param Arguments|array $options Options object or array.
 	 *
 	 * @return static Options object.
 	 */
-	public function add( array $options ) {
+	public function add( $options ) {
 
-		$this->options[] = $options;
+		$this->options[] = $options instanceof Arguments ? $options->to_array() : (array) $options;
 
 		return $this;
 	}
@@ -123,21 +152,28 @@ class Options implements Common\Arguments {
 	/**
 	 * Adds data to the options according to the given arguments.
 	 *
-	 * @since 1.0.0
+	 * @deprecated 1.1.0 Deprecated in favor of {@see self::add()} and {@see self::from_arguments()}.
+	 * @since      1.0.0
 	 *
-	 * @param Endpoint\Handler $handler Optional. Request handler object. Defaults to null.
-	 * @param Common\Arguments $args    Optional. Endpoint arguments object. Defaults to null.
-	 * @param string           $methods Optional. Comma-separated HTTP verbs. Defaults to self::DEFAULT_METHODS.
-	 * @param array            $options Optional. Additional options array. Defaults to empty array.
+	 * @param Handler   $handler Optional. Request handler object. Defaults to null.
+	 * @param Arguments $args    Optional. Endpoint arguments object. Defaults to null.
+	 * @param string    $methods Optional. Comma-separated HTTP verbs. Defaults to self::DEFAULT_METHODS.
+	 * @param array     $options Optional. Additional options array. Defaults to empty array.
 	 *
 	 * @return static Options object.
 	 */
 	public function add_from_arguments(
-		Endpoint\Handler $handler = null,
-		Common\Arguments $args = null,
+		Handler $handler = null,
+		Arguments $args = null,
 		$methods = self::DEFAULT_METHODS,
 		array $options = []
 	) {
+
+		_deprecated_function(
+			__METHOD__,
+			'1.1.0',
+			'Inpsyde\WPRESTStarter\Core\Route\Options::add() and Inpsyde\WPRESTStarter\Core\Route\Options::from_arguments()'
+		);
 
 		if ( $handler ) {
 			$options['callback'] = [ $handler, 'process' ];
@@ -159,11 +195,11 @@ class Options implements Common\Arguments {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param Endpoint\Schema $schema Schema object.
+	 * @param Schema $schema Schema object.
 	 *
 	 * @return static Options object.
 	 */
-	public function set_schema( Endpoint\Schema $schema ) {
+	public function set_schema( Schema $schema ) {
 
 		$this->options['schema'] = [ $schema, 'get_schema' ];
 
