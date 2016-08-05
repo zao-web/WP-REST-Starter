4982d72dbfb8457842b6c02511ccd3b447d603c3
tfrommen <info@tfrommen.de>
2016-08-05 08:22:00 +0200

    Introduce SchemaAwareDataFilter implementation.

---
+++ src/Core/Response/SchemaAwareDataFilter.php
@@ -0,0 +1,76 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\WPRESTStarter\Core\Response;
+
+use Inpsyde\WPRESTStarter\Common\Endpoint\Schema;
+use Inpsyde\WPRESTStarter\Common\Response\DataFilter;
+
+/**
+ * Schema-aware response data filter implementation.
+ *
+ * @package Inpsyde\WPRESTStarter\Core\Response
+ * @since   2.0.0
+ */
+final class SchemaAwareDataFilter implements DataFilter {
+
+	/**
+	 * @var array
+	 */
+	private $properties;
+
+	/**
+	 * Constructor. Sets up the properties.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param Schema $schema Schema object.
+	 */
+	public function __construct( Schema $schema ) {
+
+		$this->properties = $schema->get_properties();
+	}
+
+	/**
+	 * Returns the given response data filtered according to the given context.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param array  $data    Unfiltered response data.
+	 * @param string $context Optional. Context. Defaults to 'view'.
+	 *
+	 * @return array Filtered response data.
+	 */
+	public function filter_data( array $data, $context = 'view' ) {
+
+		foreach ( $data as $key => $value ) {
+			if ( empty( $this->properties[ $key ] ) ) {
+				continue;
+			}
+
+			$properties = $this->properties[ $key ];
+			if ( empty( $properties['context'] ) ) {
+				continue;
+			}
+
+			if ( ! in_array( $context, $properties['context'] ) ) {
+				unset( $data[ $key ] );
+
+				continue;
+			}
+
+			if ( empty( $properties['properties'] ) || 'object' !== $properties['type'] ) {
+				continue;
+			}
+
+			foreach ( $properties['properties'] as $name => $details ) {
+				if ( empty( $details['context'] ) || in_array( $context, $details['context'] ) ) {
+					continue;
+				}
+
+				unset( $data[ $key ][ $name ] );
+			}
+		}
+
+		return $data;
+	}
+}
