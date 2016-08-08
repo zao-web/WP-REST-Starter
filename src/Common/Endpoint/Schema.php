e9c8e7a11942d8c2fac1ab1bf42965e1e09754bf
tfrommen <info@tfrommen.de>
2016-08-08 09:22:16 +0200

    Add get_title() method to endpoint schema interface.

--- src/Common/Endpoint/Schema.php
+++ src/Common/Endpoint/Schema.php
@@ -9,6 +9,7 @@ use Inpsyde\WPRESTStarter\Common;
  *
  * @package Inpsyde\WPRESTStarter\Common\Endpoint
  * @since   1.0.0
+ * @since   2.0.0 Added get_title() method.
  */
 interface Schema extends Common\Schema {
 
@@ -20,4 +21,13 @@ interface Schema extends Common\Schema {
 	 * @return array Properties definition.
 	 */
 	public function get_properties();
+
+	/**
+	 * Returns the title of the schema.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @return string Title.
+	 */
+	public function get_title();
 }
