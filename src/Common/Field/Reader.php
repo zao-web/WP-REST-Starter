2e7f8358c87ca87e4f3a15e3b8bd8eaf6b48945a
tfrommen <info@tfrommen.de>
2016-08-08 07:57:25 +0200

    Add optional $object_type parameter to field reader interface.

--- src/Common/Field/Reader.php
+++ src/Common/Field/Reader.php
@@ -16,12 +16,19 @@ interface Reader {
 	 * Returns the value of the field with the given name of the given object.
 	 *
 	 * @since 1.0.0
+	 * @since 2.0.0 Added optional $object_type parameter.
 	 *
-	 * @param array           $object     Object data in array form.
-	 * @param string          $field_name Field name.
-	 * @param WP_REST_Request $request    Request object.
+	 * @param array           $object      Object data in array form.
+	 * @param string          $field_name  Field name.
+	 * @param WP_REST_Request $request     Request object.
+	 * @param string          $object_type Optional. Object type. Defaults to empty string.
 	 *
 	 * @return mixed Field value.
 	 */
-	public function get_value( array $object, $field_name, WP_REST_Request $request );
+	public function get_value(
+		array $object,
+		$field_name,
+		WP_REST_Request $request,
+		$object_type = ''
+	);
 }
